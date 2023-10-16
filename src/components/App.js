98f09bcff64730473bc5450e2e0eb761da30ac2e
Robert Bubnik <rbubnik@gmail.com>
2023-10-16 13:45:37 -0400

    passing all tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,32 @@
-// create your App component here
+import React, {useEffect, useState} from 'react'
+
+
+function App () {
+ 
+const [dogs, setDogs ] = useState (null)
+
+useEffect(()=> {
+    fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => response.json())
+    .then(data=> setDogs(data.message))
+}
+
+,[])
+
+let response = dogs === null? <p>Loading...</p> : <img alt='A Random Dog' src={dogs}/>
+return  (
+    <div>
+    {response}
+    </div>
+)
+}
+
+
+
+
+
+
+
+
+
+export default App 
